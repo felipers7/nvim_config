c1f22ee504003a0c7f15f4368bc6fec9c22776af
Felipe <felipeadiazreyes@gmail.com>
2023-06-30 22:06:14 -0400

    fixed double importation

--- lua/core/plugin_custom/qwik_scope.lua
+++ lua/core/plugin_custom/qwik_scope.lua
@@ -2,10 +2,14 @@ local function import()
 	local current_buffer = vim.api.nvim_get_current_buf()
 	local lines = vim.api.nvim_buf_get_lines(current_buffer, 0, -1, false)
 
-	
+	local is_component_imported = false
+
 	for _, line in ipairs(lines) do
+		if string.find(line, 'import component%$ from "@builder.io/qwik";') then
+			is_component_imported = true	
+		end
 		if string.find(line, 'component%$') then
-			if not string.find(line, 'import component$ from "@builder.io/qwik";') then
+			if not is_component_imported then
 				vim.api.nvim_buf_set_lines(current_buffer, 
 					0, 0, false, {'import component$ from "@builder.io/qwik";'})
 				break
