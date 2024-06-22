5613c715233ea0749118eaa148391f298aae88ee
Felipe Díaz <felipeadiazreyes@gmail.com>
2024-06-21 22:09:38 -0400

    change bufefers

--- lua/core/config.lua
+++ lua/core/config.lua
@@ -3,4 +3,7 @@ vim.opt.shiftwidth = 2
 vim.opt.expandtab = true
 vim.bo.softtabstop = 2
 vim.wo.number = true
+vim.g.mapleader = ' '
 vim.api.nvim_set_keymap('i', 'jj', '<Esc>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<leader>b', ':b#<CR>', { noremap = true, silent = true })
+
